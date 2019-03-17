c2df39c61455356b80a3ba430d14490dcde0e0d5
John <37978984+johnridesabike@users.noreply.github.com>
2019-03-17 07:36:37 -0400

    Setting up readme and license

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('app/index.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
