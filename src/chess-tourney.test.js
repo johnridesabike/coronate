a1bc0580b3ec9e2c6ea8f85a888b0af8f28c4bb1
John <37978984+johnridesabike@users.noreply.github.com>
2019-03-29 06:40:42 -0400

    prototype'd

--- src/chess-tourney.test.js
+++ src/chess-tourney.test.js
@@ -40,26 +40,26 @@ function randomRoundsDraws(tourney) {
 }
 
 const players = [
-  new Player('Matthew', 'A', 800), new Player('Mark', 'B', 850),
-  new Player('Luke', 'C', 900), new Player('John', 'D', 950),
-  new Player('Simon', 'E', 1000), new Player('Andrew', 'F', 1050),
-  new Player('James', 'G', 1100), new Player('Philip', 'H', 1150),
-  new Player('Bartholomew', 'I', 1200), new Player('Thomas', 'J', 1250),
-  new Player('Catherine', 'K', 1300), new Player('Clare', 'L', 1350),
-  new Player('Judas', 'M', 1400), new Player('Matthias', 'N', 1450),
-  new Player('Paul', 'O', 1500), new Player('Mary', 'P', 1600),
-  new Player('Theresa', 'Q', 1650), new Player('Megan', 'R', 1700),
-  new Player('Elizabeth', 'S', 1750)
+  Player('Matthew', 'A', 800), Player('Mark', 'B', 850),
+  Player('Luke', 'C', 900), Player('John', 'D', 950),
+  Player('Simon', 'E', 1000), Player('Andrew', 'F', 1050),
+  Player('James', 'G', 1100), Player('Philip', 'H', 1150),
+  Player('Bartholomew', 'I', 1200), Player('Thomas', 'J', 1250),
+  Player('Catherine', 'K', 1300), Player('Clare', 'L', 1350),
+  Player('Judas', 'M', 1400), Player('Matthias', 'N', 1450),
+  Player('Paul', 'O', 1500), Player('Mary', 'P', 1600),
+  Player('Theresa', 'Q', 1650), Player('Megan', 'R', 1700),
+  Player('Elizabeth', 'S', 1750)
 ];
 
 it('A tournament can run without crashing', () => {
-  const tourney = new Tournament('A battle for the ages', 15);
+  const tourney = Tournament('A battle for the ages', 15);
   tourney.addPlayers(players.slice(0,16));
   randomRounds(tourney);
 });
 
 it('A tournament can run with drawed rounds without crashing', () => {
-  const tourney = new Tournament('A battle for the ages', 15);
+  const tourney = Tournament('A battle for the ages', 15);
   tourney.addPlayers(players.slice(0,16));
   randomRoundsDraws(tourney);
 });
@@ -70,7 +70,7 @@ it('No players face each other more than once', () => {
     tourneyNum = 10;
   times(tourneyNum, (i) => {
     var playerOppCount = [];
-    var tourney = new Tournament();
+    var tourney = Tournament();
     tourney.addPlayers(players.slice(0,16))
     randomRounds(tourney);
     playerOppCount = playerOppCount
@@ -85,7 +85,7 @@ it('No players face each other more than once', () => {
 });
 
 it('A tournament can pair an odd number of players correctly', () => {
-  const tourney = new Tournament('An odd tournament indeed');
+  const tourney = Tournament('An odd tournament indeed');
   tourney.addPlayers(players.slice(0,19));
   randomRounds(tourney);
   var playerOppCount = tourney.roster.all
@@ -100,7 +100,7 @@ it('A tournament can pair an odd number of players correctly', () => {
 });
 
 it("A tournament doesn't crash when players are removed", () => {
-  const tourney = new Tournament();
+  const tourney = Tournament();
   tourney.addPlayers(players.slice(0,16))
   tourney.newRound().matches.forEach(match => randomMatches(match));
   tourney.newRound().matches.forEach(match => randomMatches(match));
