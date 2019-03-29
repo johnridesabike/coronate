021c29a5b2a439add64dadf302bb98b8fb2b1a0d
John <37978984+johnridesabike@users.noreply.github.com>
2019-03-29 11:44:16 -0400

    Auto-generated commit

---
+++ precache-manifest.ac0d40e28a5a285ebb857955438e4f79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3272418a6ffc84bb6b67",
+    "url": "/chessahoochee/static/js/runtime~main.362bede6.js"
+  },
+  {
+    "revision": "5580668d75f029594fa3",
+    "url": "/chessahoochee/static/js/main.47a1732d.chunk.js"
+  },
+  {
+    "revision": "5e8badf9b4f8c32ae424",
+    "url": "/chessahoochee/static/js/2.f85310b8.chunk.js"
+  },
+  {
+    "revision": "5580668d75f029594fa3",
+    "url": "/chessahoochee/static/css/main.22fa166b.chunk.css"
+  },
+  {
+    "revision": "44ae5dc14eedd52622317ad66d3ee59b",
+    "url": "/chessahoochee/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1a02a29acee67dd65a2bdb246869b61a.js"
+  "/chessahoochee/precache-manifest.ac0d40e28a5a285ebb857955438e4f79.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/chessahoochee/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
