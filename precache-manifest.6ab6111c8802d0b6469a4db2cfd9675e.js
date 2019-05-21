0574a1ab6f15148f4c62d70d06c2f1c4bb499cac
John Jackson <jjackson@CVLGA.ORG>
2019-05-21 15:42:43 -0400

    Updates

---
+++ precache-manifest.6ab6111c8802d0b6469a4db2cfd9675e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d056984db51bd05b574ef6be0859fa1f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0132334823fe7e66f446",
+    "url": "./static/css/2.fe9c9df8.chunk.css"
+  },
+  {
+    "revision": "41ce56f1936257efc5a1",
+    "url": "./static/css/main.2d7621c4.chunk.css"
+  },
+  {
+    "revision": "0132334823fe7e66f446",
+    "url": "./static/js/2.f2fac7ea.chunk.js"
+  },
+  {
+    "revision": "41ce56f1936257efc5a1",
+    "url": "./static/js/main.983be687.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "10c956d9ca89960f3b375d007669f0e3",
+    "url": "./static/media/caution.10c956d9.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ab4617fcc751d6db41c9ae27f18053f0.js"
+  "./precache-manifest.6ab6111c8802d0b6469a4db2cfd9675e.js"
 );
 
 self.addEventListener('message', (event) => {
