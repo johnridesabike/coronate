616f2e6f7e4340f8ad029ff3bab8217fcbf88d9c
John <37978984+johnridesabike@users.noreply.github.com>
2019-05-26 22:22:55 -0400

    added simple icons

--- src/components/splash.jsx
+++ src/components/splash.jsx
@@ -2,22 +2,26 @@ import Icons from "./icons";
 import React from "react";
 import styles from "./splash.module.css";
 
-export default function Splash(props) {
-    return (
-        <div className={styles.splash}>
-            <aside className={styles.hint}>
-                <Icons.ArrowUp /> Select a menu item.
-            </aside>
-            <h1 className={styles.title}>
-                <span aria-hidden={true}>♘</span>{" "}
-                Chessahoochee: <small>a chess tournament app</small>
-            </h1>
-            <footer className={styles.footer}>
+const Splash = (props) => (
+    <div className={styles.splash}>
+        <aside className={styles.hint}>
+            <Icons.ArrowUp /> Select a menu item.
+        </aside>
+        <h1 className={styles.title}>
+            <span aria-hidden={true}>♘</span>{" "}
+            Chessahoochee: <small>a chess tournament app</small>
+        </h1>
+        <footer className={styles.footer}>
+            <p>
                 Enjoy this demo.{" "}
                 <a href="https://github.com/johnridesabike/chessahoochee">
                     GitHub.
                 </a>
-            </footer>
-        </div>
-    );
-}
+            </p>
+            <p>
+                <Icons.React /> <Icons.Javascript />
+            </p>
+        </footer>
+    </div>
+);
+export default Splash;
