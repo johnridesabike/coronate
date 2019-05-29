b21fa8c79a8d9d39d557bcf04d22dc4abac030d9
John <37978984+johnridesabike@users.noreply.github.com>
2019-05-29 12:04:49 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.939ff8569fa25143c33e78865d4e6e45.js"
+  "./precache-manifest.5bd1cc928cf82a38b8d20a8272bf38cf.js"
 );
 
 self.addEventListener('message', (event) => {
